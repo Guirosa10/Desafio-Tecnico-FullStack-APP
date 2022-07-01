7b85d1423e61deba3e25a688858c70bd589d030a
Guilherme Rosa <gui.rsa@hotmail.com>
2022-07-01 19:08:55 -0300

    feat: user Registration model function created

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,1 +1,10 @@
 // user registration
+const connection = require('./connection');
+
+const userRegistration = async (email , password) => {
+	await connection.execute('INSERT INTO TodoApp.Users (email, password) VALUES (?,?)', [email, password]);
+};
+
+module.exports = {
+	userRegistration,
+};

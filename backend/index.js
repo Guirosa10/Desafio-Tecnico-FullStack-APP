e1b2904fb0569027064928f645f5d9032752680e
Guilherme Rosa <gui.rsa@hotmail.com>
2022-07-02 00:31:22 -0300

    feat: error middleware created

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,13 @@ require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+app.use((err, _req, res, _next) => {
+	if (err.status) return res.status(err.status).json({ message: err.message });
+	return res.status(500).json({ message: 'Internal Server Error' });
+});
 
 app.listen(PORT, () => {
 	console.log(`LISTENING ON PORT ${PORT}`);

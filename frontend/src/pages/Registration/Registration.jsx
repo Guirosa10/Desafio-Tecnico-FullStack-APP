3496051b78d83430fa4796a006609c35e6076728
Guilherme Rosa <gui.rsa@hotmail.com>
2022-07-03 18:24:30 -0300

    feat: registration completed, linked with application backend and working

--- frontend/src/pages/Registration/Registration.jsx
+++ frontend/src/pages/Registration/Registration.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Registration() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const submitForm = (e) => {
+    e.preventDefault();
+    const body = {
+      email,
+      password,
+    };
+    axios.post('http://localhost:4000/registration', body).then((response) => console.log(response));
+  };
+
   return (
-    <form>
+    <form onSubmit={submitForm}>
       <h1>Register</h1>
-      <input type="email" placeholder="Insert Email" />
-      <input type="password" placeholder="Insert Password" />
+      <input
+        type="email"
+        placeholder="Insert Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Insert Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        type="submit"
+      >
+        Registrar
+      </button>
     </form>
   );
 }

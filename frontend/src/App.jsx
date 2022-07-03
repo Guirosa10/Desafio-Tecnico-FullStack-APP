08d996b6ab32c2943233f1298edf4d418c1ac3a4
Guilherme Rosa <gui.rsa@hotmail.com>
2022-07-03 18:06:35 -0300

    feat: registration page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Registration from './pages/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
       <h1> TodoList Fulllstack</h1>
+      <Registration />
     </div>
   );
 }
---
+++ frontend/src/pages/Registration/Registration.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Registration() {
+  return (
+    <form>
+      <h1>Register</h1>
+      <input type="email" placeholder="Insert Email" />
+      <input type="password" placeholder="Insert Password" />
+    </form>
+  );
+}

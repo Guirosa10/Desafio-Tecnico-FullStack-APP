bafc61c6cfcff738677778dd54348ec31a979f04
Guilherme Rosa <gui.rsa@hotmail.com>
2022-07-05 02:08:10 -0300

    feat: Delete function implemented on frontend

--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import './Todo.css';
 
 export default function Todo({ todolist }) {
+  const handleDelete = () => {
+    axios.delete(`http://localhost:4000/todo/${todolist.todo_id}`);
+  };
+
   return (
     <div className="todo">
       <h4>{ todolist.todo }</h4>
       <p>{ todolist.todo_id }</p>
       <p>{ todolist.status }</p>
+      <button
+        type="button"
+        onClick={handleDelete}
+      >
+        Delete
+      </button>
     </div>
   );
 }

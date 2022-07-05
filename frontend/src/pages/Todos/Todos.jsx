0c9bf4762d036cc3f99088083a594d9e46194d90
Guilherme Rosa <gui.rsa@hotmail.com>
2022-07-04 22:45:26 -0300

    feat: todos list implemented

--- frontend/src/pages/Todos/Todos.jsx
+++ frontend/src/pages/Todos/Todos.jsx
@@ -1,13 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Todos() {
   const { id } = useParams();
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     // get from api
-  }, []);
+    axios.get(`http://localhost:4000/user/${id}`)
+      .then((response) => setTodos(response.data))
+      .catch((error) => error);
+  }, [id, todos]);
+
   return (
-    <div>{id}</div>
+    <>
+      <h2>Tasks</h2>
+      {
+        todos ? todos.map((todo) => (
+          <>
+            <h4>{ todo.todo }</h4>
+            <p>{ todo.todo_id }</p>
+            <p>{ todo.status }</p>
+          </>
+        )) : <h3>No task Found</h3>
+      }
+    </>
+
   );
 }
